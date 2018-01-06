eae074dc1e3ab01359d4d06533196113a6b6ab46
Yamabiko <ymbk@k-off.org>
2018-01-06 17:55:57 +0900

    modify templates to manage hardcorded urls by url module

--- acclist/urls.py
+++ acclist/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'acclist'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<str:username>/', views.alllist, name='alllist'),
